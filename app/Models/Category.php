0203261753ee0b0b5db996cbee81917f3f6010e9
Nuzul Abatony <albtony@gmail.com>
2023-06-06 20:00:09 +0700

    wip: create models for entities

---
+++ app/Models/Category.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'categories';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    /**
+     * Get the books associated with the category.
+     */
+    public function books()
+    {
+        return $this->belongsToMany(Book::class, 'book_category', 'category_id', 'book_id');
+    }
+}
