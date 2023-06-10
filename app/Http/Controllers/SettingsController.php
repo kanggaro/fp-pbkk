17b60715534ade46797cfba248936709279a4977
danielcristho <pepuhodaniel93.learn@gmail.com>
2023-06-10 22:10:14 +0700

    feat: add controller

---
+++ app/Http/Controllers/SettingsController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\Authenticatable;
+
+class SettingsController extends Controller
+{
+    public function index()
+    {
+        // Mengambil data pengaturan dari pengguna saat ini
+        $user = Auth::user();
+        // Mengembalikan view settings.blade.php dengan data pengguna
+        return view('settings', compact('user'));
+    }
+
+    public function update(Request $request)
+    {
+        // Validasi data yang dikirimkan oleh form pengaturan
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email,' . Auth::id(),
+            'password' => 'nullable|min:8|confirmed',
+        ]);
+
+        // Mengambil pengguna saat ini
+        $user = Auth::user();
+
+        // Mengupdate data pengguna berdasarkan input pengaturan yang dikirimkan
+        $user->name = $request->name;
+        $user->email = $request->email;
+        if ($request->password) {
+            $user->password = bcrypt($request->password);
+        }
+        $user->save();
+
+        // Redirect ke halaman pengaturan dengan pesan sukses
+        return redirect()->route('settings.index')->with('success', 'Pengaturan berhasil diperbarui.');
+    }
+}
