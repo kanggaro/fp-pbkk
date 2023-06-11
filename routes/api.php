fc6730d9914a5d257b3c67ef896b54fb9f7bfc4b
Nuzul Abatony <albtony@gmail.com>
2023-06-11 23:14:40 +0700

    feat: finish book api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,8 +20,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// users api routes
 Route::get('/users', [UserController::class, 'index']);
 Route::get('/users/{id}', [UserController::class, 'show']);
 Route::post('/users', [UserController::class, 'store']);
 Route::put('/users/{id}', [UserController::class, 'update']);
 Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+// book api routes
+Route::get('/books', [BookController::class, 'index']);
+Route::post('/books', [BookController::class, 'store']);
+Route::get('/books/search', [BookController::class, 'search']);
+Route::get('/books/{id}', [BookController::class, 'show']);
+Route::put('/books/{id}', [BookController::class, 'update']);
+Route::delete('/books/{id}', [BookController::class, 'destroy']);
+Route::get('/books/category/{category}', [BookController::class, 'getByCategory']);
+Route::get('/books/shelf/{shelf}', [BookController::class, 'getByShelf']);
+Route::get('/books/author/{author}', [BookController::class, 'getByAuthor']);
+Route::get('/books/publisher/{publisher}', [BookController::class, 'getByPublisher']);
