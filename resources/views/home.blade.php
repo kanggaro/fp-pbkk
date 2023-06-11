fabc68b74de3b07df84bdc327963a6c60527f3c4
danielcristho <pepuhodaniel93.learn@gmail.com>
2023-06-11 21:43:08 +0700

    feat: update homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,19 +11,26 @@
 <body class="bg-gray-100">
     <div class="flex justify-center items-center h-screen">
         <div class="max-w-md w-full px-6 py-8 bg-white rounded-lg shadow-md">
-            <h2 class="text-2xl font-semibold text-gray-800 mb-6">Welcome to Library Management System</h2>
+            <h2 class="text-2xl font-semibold text-gray-800 mb-">Welcome to Library Management System</h2>
 
-            <div class="flex justify-between">
-                <a href="/login"
-                    class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                    Login
-                </a>
-                <a href="/register"
-                    class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                    Register
-                </a>
+            <div class="flex flex-col items-center space-y-4">
+                <div class="flex space-x-6">
+                    <a href="/login"
+                        class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                        Login
+                    </a>
+                    <a href="/register"
+                        class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                        Register
+                    </a>
+                </div>
+                <!-- <a href="/admin-login"
+                    class="text-sm text-gray-500 hover:text-gray-700 font-semibold underline">
+                    Login as Admin
+                </a> -->
             </div>
         </div>
     </div>
 </body>
+
 </html>
